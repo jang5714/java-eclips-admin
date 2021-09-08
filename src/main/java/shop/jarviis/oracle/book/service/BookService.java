d621db20aa9c03cbe3052a94d75118ea8b6d60c9
jang5714 <lionx3@naver.com>
2021-09-08 18:10:59 +0900

    0908

---
+++ src/main/java/shop/jarviis/oracle/book/service/BookService.java
@@ -0,0 +1,8 @@
+package shop.jarviis.oracle.book.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface BookService {
+
+}
---
+++ src/main/java/shop/jarviis/oracle/customer/mapper/CustomerMapper.java
@@ -0,0 +1,8 @@
+package shop.jarviis.oracle.customer.mapper;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerMapper {
+
+}
